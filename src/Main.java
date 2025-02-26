9ab2cc3b55f864c365bc11465e639e592dbf5068
Svyatoslav Starkov <svyatoslav.starkov@yandex.ru>
2025-02-26 22:44:39 +0500

    Add main

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        int initialBalance = 100;
+        int replenishmentOne  = 300;
+        int replenishmentTwo = 1100;
+
+        int bonus = 0;
+        if (replenishmentOne > 1000) {
+            bonus = replenishmentOne / 100;
+        }
+        if (replenishmentTwo > 1000) {
+            bonus = replenishmentTwo / 100;
+        }
+        int clientAccount = initialBalance + replenishmentOne + replenishmentTwo + bonus;
+
+
+        System.out.println("Итоговый баланс: " + clientAccount + " рублей.");
+        System.out.println("Количество бонусных рублей " + bonus);
+    }
+}
