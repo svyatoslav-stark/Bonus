70d473ecdb0a7c2e72321a0e3b8d293b9743ed50
Svyatoslav Starkov <svyatoslav.starkov@yandex.ru>
2025-03-03 12:24:51 +0500

    Сhanges main

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,13 @@
 public class Main {
     public static void main(String[] args) {
         int initialBalance = 100;
-        int replenishmentOne = 300;
-        int replenishmentTwo = 1100;
+        int replenishment = 1100;
 
         int bonus = 0;
-        if (replenishmentOne > 1000) {
-            bonus = replenishmentOne / 100;
+        if (replenishment > 1000) {
+            bonus = replenishment / 100;
         }
-        if (replenishmentTwo > 1000) {
-            bonus = replenishmentTwo / 100;
-        }
-        int clientAccount = initialBalance + replenishmentOne + replenishmentTwo + bonus;
+        int clientAccount = initialBalance + replenishment + bonus;
 
 
         System.out.println("Итоговый баланс: " + clientAccount + " рублей.");
